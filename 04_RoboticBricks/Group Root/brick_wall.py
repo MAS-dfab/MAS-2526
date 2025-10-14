e51094858daf9a59d3d37abcf36d0c722bcca04d
minghip <minghiparkeur@gmail.com>
2025-10-14 15:35:11 +0200

    minor change to transform

--- 04_RoboticBricks/Group Root/brick_wall.py
+++ 04_RoboticBricks/Group Root/brick_wall.py
@@ -9,9 +9,9 @@ from compas.geometry import Box
 # and also has a rotate behavior that accepts degrees of rotation
 class Brick:
     # Class attribute for default brick dimensions
-    LENGTH = 20
-    WIDTH = 10
-    HEIGHT = 5
+    LENGTH = 32
+    WIDTH = 15
+    HEIGHT = 10
 
     def __init__(self, frame=None):
         self.length = Brick.LENGTH
@@ -34,10 +34,10 @@ class Brick:
         pick_frame = self.frame.copy()
         pick_frame.point.z += self.height / 2
 
-    def transform(self, Transformation):
-        self.frame.transform(Transformation)
-
         #flip orientation of frame if pointing 'up'.
         if pick_frame.zaxis.z > 0:
             pick_frame.xaxis = -pick_frame.xaxis
         return pick_frame
+    
+    def transform(self, Transformation):
+        self.frame.transform(Transformation)
