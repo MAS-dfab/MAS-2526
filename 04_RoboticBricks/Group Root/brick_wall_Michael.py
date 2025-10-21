66fa2c121cadaa0a7b20a3987c177e79982af833
Michael Park <minpar@student.ethz.ch>
2025-10-21 15:50:56 +0200

    updated

---
+++ 04_RoboticBricks/Group Root/brick_wall_Michael.py
@@ -0,0 +1,70 @@
+import math
+from compas.geometry import Frame
+from compas.geometry import Transformation
+from compas.geometry import Box
+
+# There's a data type called Brick
+# that has attributes: length, width, height and a point
+# and also has a rotate behavior that accepts degrees of rotation
+class Brick:
+    # Class attribute for default brick dimensions
+<<<<<<< HEAD:04_RoboticBricks/Group Root/brick_wall.py
+    LENGTH = 32
+    WIDTH = 15
+    HEIGHT = 10
+=======
+    LENGTH = 32.02
+    WIDTH = 15.2
+    HEIGHT = 9.94
+>>>>>>> 8fa9136aaa919f4135eb1c35cf8ebb906c09dd3d:04_RoboticBricks/brick_wall.py
+
+    def __init__(self, frame=None):
+        self.length = Brick.LENGTH
+        self.width = Brick.WIDTH
+        self.height = Brick.HEIGHT
+        self.frame = frame
+
+    def __str__(self):
+        return f"Brick of {self.length}cm x {self.width}cm x {self.height}cm (frame: {self.frame})"
+
+    def draw(self):
+        box = Box(self.length, self.width, self.height)
+        box.frame = self.frame.copy()
+        return box
+
+    def rotate(self, rotation_in_degrees):
+        self.frame.rotate(math.radians(rotation_in_degrees), (0, 0, 1), self.frame.point)
+
+    def get_pick_frame(self):
+        pick_frame = self.frame.copy()
+        pick_frame.point.z += self.height / 2
+
+        #flip orientation of frame if pointing 'up'.
+        if pick_frame.zaxis.z > 0:
+            pick_frame.xaxis = -pick_frame.xaxis
+        return pick_frame
+<<<<<<< HEAD:04_RoboticBricks/Group Root/brick_wall.py
+    
+    def transform(self, Transformation):
+        self.frame.transform(Transformation)
+
+
+    #changes!!
+=======
+
+class BigBrick(Brick):
+    LENGTH = 240.0
+    WIDTH = 115.0
+    HEIGHT = 52.0
+    def __init__(self, frame=None):
+        super().__init__(frame)
+        self.length = BigBrick.LENGTH
+        self.width = BigBrick.WIDTH
+        self.height = BigBrick.HEIGHT
+    
+    def get_pick_frame(self):
+        pick_frame = self.frame.translated((0, 0, -self.height / 2))
+        if pick_frame.zaxis.z > 0:
+            pick_frame.xaxis = -pick_frame.xaxis
+        return pick_frame
+>>>>>>> 8fa9136aaa919f4135eb1c35cf8ebb906c09dd3d:04_RoboticBricks/brick_wall.py
