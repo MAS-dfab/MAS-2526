9d0ac909db3dac1d988457dae4fdd4819ae39e82
nadyamilas <nmilas@ethz.ch>
2025-12-02 09:53:35 +0100

    changes

--- 08_SpatialAssemblies/StudentProjects/group_one/group_one_scripts/nadja_studies_25_12_01_r1.py
+++ 08_SpatialAssemblies/StudentProjects/group_one/group_one_scripts/nadja_studies_25_12_01_r1.py
@@ -32,7 +32,7 @@ class StickModule:
         self.point = plane.point
         self.sticks = []
 
-    
+        
     def CreateModule(self, angle_a = 45, angle_b = 45, length = 2, sticks_distance = 5):
         """
         Docstring for CreateModule
@@ -41,16 +41,17 @@ class StickModule:
         :return: two lines
         """
         # line initial planes that are rotated
-        point_a1 = self.point
-        point_a2 = rotate_move(self.point, self.frame, math.radians(angle_a), "y", "z",length)
+        point_a = self.point
+        plane_a = Frame(point_a, self.frame.xaxis, self.frame.yaxis)
+        plane_a.rotate(math.radians(angle_a))
         
-        point_b = rotate_move(self.point, self.frame, math.radians(0), 0, "x",sticks_distance)
-        point_b_translate = rotate_move(point_b, self.frame, math.radians(0), 0, "y",Stick.WIDTH)
-        point_b2 = rotate_move(point_b_translate, self.frame, math.radians(-angle_b), "y", "z",length)
+        point_b = point_a.translated((self.frame.yaxis * Stick.WIDTH) + (self.frame.xaxis * sticks_distance))
+        plane_b = Frame(point_a, self.frame.xaxis, self.frame.yaxis)
+        plane_b.rotate(math.radians(-angle_b))
 
         # create lines
-        line_a = Line(point_a1, point_a2)
-        line_b = Line(point_b_translate, point_b2)
+        line_a = Line.from_point_and_vector(point_a, plane_a.zaxis * length)
+        line_b = Line.from_point_and_vector(point_b, plane_b.zaxis * length)
         
         # use stick class
         s1 = Stick(line_a)
@@ -59,6 +60,31 @@ class StickModule:
         stick2 = s2.geometry
         
         return [stick1, stick2]
-
-    # def get_module_faces(self):
+    
+    
+        # def CreateModule(self, angle_a = 45, angle_b = 45, length = 2, sticks_distance = 5):
+    #     """
+    #     Docstring for CreateModule
         
+    #     :param self: angle of the stick one and stick two and legth
+    #     :return: two lines
+    #     """
+    #     # line initial planes that are rotated
+    #     point_a1 = self.point
+    #     point_a2 = rotate_move(self.point, self.frame, math.radians(angle_a), "y", "z",length)
+        
+    #     point_b = rotate_move(self.point, self.frame, math.radians(0), 0, "x",sticks_distance)
+    #     point_b_translate = rotate_move(point_b, self.frame, math.radians(0), 0, "y",Stick.WIDTH)
+    #     point_b2 = rotate_move(point_b_translate, self.frame, math.radians(-angle_b), "y", "z",length)
+
+    #     # create lines
+    #     line_a = Line(point_a1, point_a2)
+    #     line_b = Line(point_b_translate, point_b2)
+        
+    #     # use stick class
+    #     s1 = Stick(line_a)
+    #     s2 = Stick(line_b)
+    #     stick1 = s1.geometry
+    #     stick2 = s2.geometry
+        
+    #     return [stick1, stick2]
