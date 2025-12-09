557c9dc519b6d4b6091270a03f910db0dca45bea
Juste <gatjuste@gmail.com>
2025-12-09 09:24:31 +0100

    latest_JG

---
+++ 08_SpatialAssemblies/StudentProjects/group_two/Juste/_Archives/02_/stick_branch.py
@@ -0,0 +1,101 @@
+from compas.geometry import Line, Frame, Vector
+from compas.geometry import Rotation
+import math
+
+from Sticks import Stick
+
+class BranchingModule:
+    def __init__(self, root_frame, stick_length=None, width=None, depth=None):
+        """
+        Constructor for Branching module.
+        
+        Args:
+            root_frame: Frame from which tree will grow
+            stick_length: Length of each stick
+            width: Width of sticks (defaults to Stick.WIDTH)
+            depth: Depth of sticks (defaults to Stick.DEPTH)
+        """
+        self.root_frame = root_frame
+        self.sticks = []
+        self.stick_length = stick_length
+        self.width = width or Stick.WIDTH
+        self.depth = depth or Stick.DEPTH
+
+        self._init_first_stick(root_frame)
+
+    def _init_first_stick(self, frame):
+        """
+        Private method for creating the first stick.
+        
+        Args:
+            frame: Frame from which stick will grow
+        """
+        # Draw line based on start frame
+        stick_axis = Line.from_point_and_vector(frame.point, frame.zaxis * self.stick_length)
+
+        # Create stick 
+        st_stick = Stick(stick_axis, z_vector = frame.yaxis)
+
+        # Add stick to list of sticks
+        self.sticks.append(st_stick)
+
+    def get_face_frame(self, stick_index, face_index):
+        """
+        Gets a frame on one of the four faces of a stick.
+        Args:
+            stick_index: Index of the stick
+            face_index: Face index (0-3) around the stick
+
+        Returns:
+            Frame on the specified face
+        """
+
+        # Rotate stick frame based on index
+        stick_frame = self.sticks[stick_index].frame
+        angle = face_index * math.pi/2
+        R = Rotation.from_axis_and_angle(stick_frame.xaxis, angle, stick_frame.point)
+        new_frame = stick_frame.transformed(R)
+        new_frame.point = self.sticks[stick_index].axis.end
+
+        # Offset frame to be on surface on stick
+        new_frame.point += new_frame.yaxis * self.depth/2
+
+        return new_frame
+         
+    def grow_stick(self, from_stick_index = -1, face_index = 0, angle = 0.0, offset = 0.0):
+        """
+        Grows a new stick from an existing stick.
+        
+        Args:
+            from_stick_index: Index of stick to grow from 
+            face_index: Index of the face to grow from (0-3)
+            angle: Angle of rotation in radians
+        """
+                
+        # Get position on original stick
+        position = self.get_face_frame(from_stick_index, face_index).copy()
+        position.point += position.yaxis * self.depth/2
+        position.point += -position.xaxis * offset
+        
+        # Rotate along face frame
+        R = Rotation.from_axis_and_angle(position.yaxis, math.radians(angle), point = position.point)
+        position.transform(R)
+
+        # Offset along axis
+        position.point += -position.xaxis * offset
+        
+        # Create new stick
+        axis = Line.from_point_and_vector(position.point, position.xaxis * self.stick_length)
+        z_vector = position.yaxis
+
+        new_stick = Stick(axis, z_vector)
+        self.sticks.append(new_stick)
+
+    def visualize(self):
+        """
+        Returns all stick geometries.
+        
+        Returns:
+            List of Box geometries
+        """
+        return [stick.geometry for stick in self.sticks]
---
+++ 08_SpatialAssemblies/StudentProjects/group_two/Juste/_Archives/02_/stick_root.py
@@ -0,0 +1,93 @@
+from compas.geometry import Line
+from compas.geometry import Rotation
+import math
+
+from Sticks import Stick
+
+class RootModule:
+    def __init__(self, branches, segment_index, stick_length=None, width=None, depth=None):
+        """
+        Constructor for Root module.
+        ß
+        Args:
+            branches: list of frames from existing tree
+            segment_index = index of frame for new root
+
+            stick_length: Length of each stick
+            width: Width of sticks (defaults to Stick.WIDTH)
+            depth: Depth of sticks (defaults to Stick.DEPTH)
+        """
+
+        self.branch_frames = [b.frame for b in branches]
+        self.segment_index = segment_index
+        self.sticks = []
+
+        self.stick_length = stick_length
+        self.width = width or Stick.WIDTH
+        self.depth = depth or Stick.DEPTH
+
+        self.segment_frame = self.get_segment_frame()
+
+    def get_segment_frame(self):
+        #get segment_frame based on segment_index
+        frame = self.branch_frames[self.segment_index].copy()
+
+        return frame
+
+    def get_root_frame(self, face_index, rotation_angle = 0.0, segment_offset = 0.0):
+        """
+        Create root frame
+        Translate root frame on one of the four faces of a Stick
+        Create stick from frame
+
+        Args:
+            frames: List of frames
+            Index: Index of frame for root frame
+            face_index: Face index (0-3) around the stick
+
+        Returns:
+        Frame on the specified face
+        """
+
+        """translate root axis to be adjacent to selected segment"""
+
+        #Rotate stick frame based on index
+        face_angle = face_index * math.pi/2
+        R_face = Rotation.from_axis_and_angle(self.segment_frame.xaxis, face_angle, self.segment_frame.point)
+        translated_frame = self.segment_frame.transformed(R_face)
+        #translate frame to end of self.segment_frame
+        translated_frame.point += translated_frame.xaxis * self.stick_length/2
+
+        #Offset frame to be on surface on stick
+        translated_frame.point += translated_frame.yaxis * self.depth
+
+        #Offset along segment axis
+        translated_frame.point += -translated_frame.xaxis * segment_offset
+
+        #Rotate along face frame
+        R_stick = Rotation.from_axis_and_angle(translated_frame.yaxis, math.radians(rotation_angle), point = translated_frame.point)
+        translated_frame.transform(R_stick)
+
+        return translated_frame
+
+    def get_root_stick(self, root_frame, root_offset = 0.0):
+
+        # Offset along root axis
+        root_frame.point += -root_frame.xaxis * root_offset
+
+        #Create translated axis
+        translated_axis = Line.from_point_and_vector(root_frame.point, root_frame.xaxis * self.stick_length)
+
+        #create root stick using root axis
+        root_stick = Stick(translated_axis, z_vector = root_frame.yaxis)
+
+        self.sticks.append(root_stick)
+
+    def visualize(self):
+        """
+        Returns all stick geometries.
+        
+        Returns:
+            List of Box geometries
+        """
+        return [stick.geometry for stick in self.sticks]
