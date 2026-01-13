49544abff5226c91e639b7d7dca3b065ebe82a41
gatjuste <75351195+gatjuste@users.noreply.github.com>
2026-01-13 13:24:36 +0100

    _bmw

--- 09_BMWHollowCore/src/PrintClassEndSP.py
+++ 09_BMWHollowCore/src/PrintClassEndSP.py
@@ -74,10 +74,10 @@ class PrintPath:
         self.printpoints.insert(0, safe_pt)
     
     #add end safety point
-    def add_end_safety_point(self, vector, safety_distance = 550.0):
+    def add_end_safety_point(self, vector, safety_distance = 50.0):
         vec = vector * safety_distance
         T = Translation.from_vector(vec)
-        TT = Translation.from_vector(Vector(0, 0, 550))
+        TT = Translation.from_vector(Vector(500, 0, 50))
         tail_pt = PrintPoint(self.printpoints[-1].point.transformed(T), toggle = True, layer_idx = self.printpoints[-1].layer_idx)
         safe_pt = PrintPoint(tail_pt.point.transformed(TT), velocity=18.0, toggle=True, layer_idx=self.printpoints[-1].layer_idx)
         self.printpoints.append(tail_pt)
